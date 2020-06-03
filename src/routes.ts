94de7a64726560e4e3b92d6758aa09a531e4cd20
vitor <vitorn2009@hotmail.com>
2020-06-02 21:19:43 -0300

    Criação de controllers para implementação dos métodos de acordo com as rotas
    Ajuste no arquivo de rotas apontando para os controllers
    
    Ajuste no arquivo server importando novas bibliotecas e liberando acesso via rota áa pasta uploads

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+    
 const routes = express.Router();
 
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-routes.get('/', (request, response) => {
-    return response.json({messenge: 'hello'})
-})
+routes.get('/items', itemsController.index)
 
-
-
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
 
 export default routes
